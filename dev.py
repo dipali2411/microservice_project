0c36c40c221e5fe41fd9360dd22db8327c62079c
dipali2411 <112314590+dipali2411@users.noreply.github.com>
2022-12-06 11:58:17 +0530

    Add files via upload

---
+++ dev.py
@@ -0,0 +1,67 @@
+start = "22 09 * * *"
+stop = "23 09 * * *"
+
+file = open('another-autoscaler-start.conf',"w")
+file.write(f'''
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: cron
+data:
+  another-autoscaler/start-time: "{start}"
+  another-autoscaler/stop-time: "{stop}"
+  another-autoscaler/start-replicas: "2"
+  another-autoscaler/stop-replicas: "0"
+ ''')
+
+file = open('deploy1.yaml',"w")
+file.write(f'''
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nginx
+  labels:
+    app: nginx
+  annotations:
+   another-autoscaler/start-time: "{start}"
+   another-autoscaler/stop-time: "{stop}"
+   another-autoscaler/start-replicas: "2"
+   another-autoscaler/stop-replicas: "0"
+spec:
+  replicas: 0
+  selector:
+    matchLabels:
+       app: nginx
+  template:
+    metadata:
+       labels:
+         app: nginx
+    spec:
+      containers:
+      - name: nginx
+        image: nginx:latest
+        ports:
+        - containerPort: 80
+        # env:
+        #    - name: another-autoscaler-start
+        #      valueFrom:
+        #          configMapKeyRef:  
+        #             name: cron
+        #             key: another-autoscaler/start-time
+        #    - name: another-autoscaler-stop
+        #      valueFrom:
+        #          configMapKeyRef:  
+        #             name: cron
+        #             key: another-autoscaler/stop-time
+        #    - name: another-autoscaler-start
+        #      valueFrom:
+        #          configMapKeyRef:  
+        #             name: cron
+        #             key: another-autoscaler/start-replicas
+        #    - name: another-autoscaler-stop
+        #      valueFrom:
+        #          configMapKeyRef:  
+        #             name: cron
+        #             key: another-autoscaler/stop-replicas
+
+ ''')
